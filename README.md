db6b7f95aba1b0652afafa7e21383eb781bb8953
KGJ <aplnosun@gmail.com>
2015-01-01 18:58:50 +0900

    initialize

--- README.md
+++
@@ -1,4 +0,0 @@
-aplnosun.github.com
-===================
-test 항목 추가
-이것은 http 프로토콜을 이용한 push입니다.
--- include/file_maker.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-
-#define MAX     100000
-#define REPEAT  100
